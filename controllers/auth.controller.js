5f38ddc187e88d6c6cb58925c2852cb454b2ebe6
bezbrain <belloquadriolawale@gmail.com>
2023-12-21 20:18:35 +0100

    Sign a token for registration

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const UserCollection = require("../models/Users");
 const { StatusCodes } = require("http-status-codes");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   // const { password } = req.body;
@@ -16,9 +17,14 @@ const register = async (req, res) => {
 
   const user = await UserCollection.create({ ...req.body });
 
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+
   res.status(StatusCodes.CREATED).json({
     success: true,
-    user,
+    user: { name: user.name },
+    token,
     message: "User created successfully",
   });
 };
