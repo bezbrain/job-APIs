2860a89474559fa3c24b8e12bb1333425f7b4158
bezbrain <belloquadriolawale@gmail.com>
2023-12-24 15:58:36 +0100

    Set up the single job request api

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -4,11 +4,34 @@ const BadRequestError = require("../errors/bad-request");
 const NotFoundError = require("../errors/not-found");
 
 const getAllJobs = async (req, res) => {
-  //
+  const jobs = await JobCollection.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: "All jobs fetched",
+    jobsLength: jobs.length,
+    jobs,
+  });
 };
 
 const getSingleJob = async (req, res) => {
-  res.send("Single Job");
+  const {
+    user: { userId },
+    params: { jobID },
+  } = req;
+  const job = await JobCollection.findOne({ _id: jobID, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`Job with the id, ${jobID} does not exist`);
+  }
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Single job fetched successfully",
+    job,
+  });
 };
 
 const createJob = async (req, res) => {
