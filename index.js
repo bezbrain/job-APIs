c34724f2a8033def7230cdd1ec686e2e9b9fbb3e
Bello Quadri Olawale <belloquadriolawale@gmail.com>
2024-10-05 12:19:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ require("dotenv").config();
 const app = express();
 
 // Extra security packages
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
+// const xss = require("xss-clean");
+// const rateLimiter = require("express-rate-limit");
 
 // Swagger
 const swaggerUI = require("swagger-ui-express");
@@ -32,19 +32,19 @@ const authRouter = require("./routes/auth.route");
 const jobRouter = require("./routes/jobs.routes");
 const authMiddleware = require("./middleware/auth");
 
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-    // store: ... , // Use an external store for consistency across multiple server instances.
-  })
-);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+//     standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+//     // store: ... , // Use an external store for consistency across multiple server instances.
+//   })
+// );
 app.use(express.json());
-app.use(helmet());
+// app.use(helmet());
 app.use(cors());
-app.use(xss());
+// app.use(xss());
 
 // The dynamic port
 const port = process.env.PORT || 3000;
